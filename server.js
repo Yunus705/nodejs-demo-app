b9bce9c042c15409a3a6823e97075897cb5bad19
yunus <yunussharif705@gmail.com>
2025-04-07 18:21:51 +0530

    updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js App with CI/CD 🚀");
+  res.send("Hello from Yunus");
 });
 
 app.listen(port, () => {
